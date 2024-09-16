bb95d8744e94e757a92abbb5088e60b7463bd56c
Sphiyo <niko6ix9ine@gmail.com>
2024-09-16 18:38:07 +0100

    Test should be reverted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./style.css";
 import Header from "./components/header/Header";
+import { Modal, Button } from "react-bootstrap";
+import { useState } from "react";
+
+const data = {};
 
 export default function App() {
+  const [show, setShow] = useState(false);
+  const handleShowModal = () => setShow(true);
+  const handleCloseModal = () => setShow(false);
+
   return (
+  <>
     <Header />
+    <Button onClick={handleShowModal}> Click mee</Button>
+    <Modal 
+      show={show} 
+      onHide={handleCloseModal}
+      centered={true}
+      size="lg"
+
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Body</Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleCloseModal}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  </>
   );
 }
 
